c46ec8307ad7ee738b18c259edc11b062b5e73ff
catzilla <catzillaorz@outlook.com>
2023-03-14 18:33:25 +0800

    feat: canvas drawLine

--- src/views/canvas/line/index.vue
+++ src/views/canvas/line/index.vue
@@ -1,5 +1,41 @@
 <template>
   <div>
-   line of canvas 
+    line of canvas
+    <canvas ref="canvas" id="canvas"></canvas>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'drawLine',
+
+  data() {
+    return {
+      cnv: null,
+      cxt: null
+
+    }
+  },
+
+  mounted() {
+    debugger
+    this.$nextTick(() => {
+      this.cnv = document.getElementById('canvas')
+      this.cxt = this.cnv.getContext('2d')
+      this.drawLine()
+    })
+
+  },
+  methods: {
+    $$(id) {
+      return document.getElementById(id)
+    },
+    drawLine() {
+      this.cxt.moveTo(50, 100)
+      this.cxt.lineTo(150, 50)
+      this.cxt.stoke()
+
+    }
+  },
+}
+</script>

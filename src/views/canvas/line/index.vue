dc33625e977946e43754b3c9f89233b6483a2ff7
catzilla <catzillaorz@outlook.com>
2023-03-15 10:26:57 +0800

    feat: clean code

--- src/views/canvas/line/index.vue
+++ src/views/canvas/line/index.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script>
-import {CanvasService} from '@/views/canvas/service/canvasService.js'
+import { CanvasService } from '@/views/canvas/service/canvasService.js'
 export default {
   name: 'drawLine',
 
   data() {
     return {
-      canvasService:null
+      canvasService: null
     }
   },
 
@@ -22,14 +22,6 @@ export default {
 
   },
   methods: {
-    $$(id) {
-      return document.getElementById(id)
-    },
-    drawLine() {
-      this.cxt.moveTo(50, 100)
-      this.cxt.lineTo(150, 50)
-      this.cxt.stroke()
-    }
   },
 }
 </script>
